a526836d3ab2ffc3227dd1e91b6e5fdde1869c80
chuanwenwang <chuanwenwang@sohu-inc.com>
2018-01-09 18:11:16 +0800

    热乎添加文件

---
+++ LearnAbout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LearnAbout
+//
+//  Created by wangchuanwen on 2018/1/9.
+//  Copyright © 2018年 wangchuanwenchuawen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LearnAbout/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  LearnAbout
+//
+//  Created by wangchuanwen on 2018/1/9.
+//  Copyright © 2018年 wangchuanwenchuawen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
